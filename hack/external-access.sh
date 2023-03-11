5e67e3bfe255bd09bbc311d92438bb19aa860cae
Pedro Tashima <pedrotashima42@gmail.com>
2023-03-10 21:39:16 -0300

    remove certificate from script

--- hack/external-access.sh
+++ hack/external-access.sh
@@ -3,14 +3,14 @@
 DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 WORKDIR="$DIR/../k8s/external-access"
 # api
-kubectl apply -f "$WORKDIR/cert-manager.yaml"
+#kubectl apply -f "$WORKDIR/cert-manager.yaml"
 kubectl apply -f "$WORKDIR/awa-api-ingress.yaml"
 kubectl apply -f "$WORKDIR/awa-api-redirect.yaml"
 kubectl apply -f "$WORKDIR/awa-api-web.yaml"
 kubectl apply -f "$WORKDIR/awa-api-certificate.yaml"
 kubectl apply -f "$WORKDIR/awa-api-ingressroute.yaml"
 # admin
-kubectl apply -f "$WORKDIR/awa-admin-cert-manager.yaml"
+#kubectl apply -f "$WORKDIR/awa-admin-cert-manager.yaml"
 kubectl apply -f "$WORKDIR/awa-admin-ingress.yaml"
 kubectl apply -f "$WORKDIR/awa-admin-redirect.yaml"
 kubectl apply -f "$WORKDIR/awa-admin-web.yaml"
